a3171551204479adf060219749da0a11cb0aa847
Dan Bazhanov <daniil.bazhanov@vizientinc.com>
2023-03-27 16:36:50 +0300

    Added Microsoft DI providers

---
+++ LoggingLibrary/MicrosoftDI/LoggingConfiguration.cs
@@ -0,0 +1,8 @@
+namespace LoggingLibrary;
+
+public class LoggingConfiguration
+{
+    public string LogsFilePath { get; set; }
+    
+    public int LogEntriesWritesForReplicationThreshold { get; set; }
+}
---
+++ LoggingLibrary/MicrosoftDI/MicrosoftDependencyInjectionExtension.cs
@@ -0,0 +1,30 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+namespace LoggingLibrary.MicrosoftDI;
+
+public static class MicrosoftDependencyInjectionExtension
+{
+    private const string DefaultLogFilePath = "logger.configuration.json";
+
+    private const int DefaultWritesThresholdToReplication = 100;
+    public static IHostBuilder AddLogging(this IHostBuilder hostBuilder,
+        Action<LoggingConfiguration> configureLogging)
+    {
+        var loggingConfiguration = new LoggingConfiguration
+        {
+            LogsFilePath = DefaultLogFilePath,
+            LogEntriesWritesForReplicationThreshold = DefaultWritesThresholdToReplication 
+        };
+        configureLogging(loggingConfiguration);
+        hostBuilder.ConfigureAppConfiguration((_, configurationBuilder) =>
+        {
+            configurationBuilder.AddJsonFile(loggingConfiguration.LogsFilePath, optional:false);
+        }).ConfigureServices((_, serviceCollection) =>
+        {
+            
+        });
+
+        return hostBuilder;
+    }
+}
